b84e47f0bf1658a89e3b87f676340c6c568350df
Sireesha <n210413@rguktn.ac.in>
2024-12-20 09:34:42 -0800

    hi

---
+++ src/Components/Card.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+const Card=()=>{
+    return(
+<div>
+    <div className="bg-black h-[300px] w-[400px]">
+<div>hi guys</div>
+    </div>
+</div>
+    );
+}
+export default Card;
